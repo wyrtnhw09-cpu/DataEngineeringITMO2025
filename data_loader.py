ecc47d00d5438f22dae714ec2c95e739ecfd18cc
Julia Bazhenova <wyrtnh.w09@gmail.com>
2025-09-23 19:11:55 +0700

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,6 @@
-
+# скрипт выгрузки файла из Google Drive:
+import pandas as pd
+FILE_ID = "1eUWO6tAac-u-a1IrIqXJWgKE_4JJyjWt"
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
+raw_data = pd.read_csv(file_url)
+print(raw_data.head(10)) # выводим первые 10 строк датасета
